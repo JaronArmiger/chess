bd8b234a8dd0b9b5f4ae3ffee11ac4b80b178688
JaronArmiger <jaron.armiger@gmail.com>
2020-03-21 14:30:06 -0500

    add test for Board#occupied?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,21 @@
 require './lib/board.rb'
 
 describe Board do
-	before { @board = Board.new }
+	before { 	@board = Board.new 
+				}
 	describe "#translate_pos" do
 		it "converts traditional chess position notation to array-readable notation" do
 			expect(@board.translate_pos("a1")).to eql([0,0])
+		end
+	end
+
+	describe "#occupied?" do
+		it "returns true if there is a piece at given position" do
+			@board.field[3][3] = "\u{2655}"
+			expect(@board.occupied?([3,3])).to be true
+		end
+		it "returns false if the given position is vacant" do
+			expect(@board.occupied?([3,3])).to be false
 		end
 	end
 end
