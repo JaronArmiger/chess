18b425e0a89b2482129542cb6afc258efa7654f3
JaronArmiger <jaron.armiger@gmail.com>
2020-03-21 14:24:22 -0500

    add #occupy and create separate vars arr_pos and board_pos

--- lib/board.rb
+++ lib/board.rb
@@ -48,39 +48,64 @@ class Board
 		puts ""
 	end
 
-	def translate_pos(pos)
-		letter = pos[0]
-		num = pos[1].to_i
-		pos_arr = [letter.ord - 97, num - 1]
+	def translate_pos(board_pos)
+		letter = board_pos[0]
+		num = board_pos[1].to_i
+		arr_pos = [letter.ord - 97, num - 1]
 	end
 
 	def populate(pieces_hash)
 		pieces_hash.each do |name, piece|
-			pos = translate_pos(piece.pos)
-			field[pos[0]][pos[1]] = piece.sym
+			arr_pos = translate_pos(piece.pos)
+			field[arr_pos[0]][arr_pos[1]] = piece.sym
 		end
 	end
 
-	def valid_moves(piece)
+	def occupied?(arr_pos)
+		square = field[arr_pos[0]][arr_pos[1]]
+		if square.nil?
+			return false
+		else
+			return true
+		end
+	end
+
+	def valid_moves(piece,board_pos)
 		if piece.is_a? Pawn
-			moves = pawn_moves
+			moves = pawn_moves(board_pos)
 		elsif piece.is_a? Rook
-			moves = rook_moves
+			moves = rook_moves(board_pos)
 		elsif piece.is_a? Knight
-			moves = knight_moves
+			moves = knight_moves(board_pos)
 		elsif piece.is_a? Bishop
-			moves = bishop_moves
+			moves = bishop_moves(board_pos)
 		elsif piece.is_a? Queen
-			moves = queen_moves
+			moves = queen_moves(board_pos)
 		elsif piece.is_a? King
-			moves = king_moves
+			moves = king_moves(board_pos)
 		end
 		moves
 	end
 
+	def pawn_moves(board_pos)
+		moves = []
 
+	end
 
+	def rook_moves(board_pos)
+	end
 
+	def knight_moves(board_pos)
+	end
+
+	def bishop_moves(board_pos)
+	end
+
+	def queen_moves(board_pos)
+	end
+
+	def king_moves(board_pos)
+	end
 
 
 end
