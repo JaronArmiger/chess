bc874204148245c8708b94571e30f2a8d69dc475
JaronArmiger <jaron.armiger@gmail.com>
2020-03-22 21:34:08 -0500

    outsource Game#choose_pos and Game#get_chosen_arr to declutter Game#turn

--- lib/game.rb
+++ lib/game.rb
@@ -31,41 +31,69 @@ class Game
 		@board.show
 	end
 
-	def get_chosen_hash
+	def get_chosen_arr(player)
 		while 1
 			puts "which piece do you want to move?"
-			piece = gets.chomp.downcase
-			if valid_piece_name?(piece)
-				if player.pieces.keys.any? { |name| name =~ /#{piece}/ }
-					puts "okay! you have that piece"
-					chosen_hash = {}
-					player.pieces.each do |piece_name,piece_object|
-						if piece_name =~ /#{piece}/
-							#print "piece_name, piece"; p piece_name; p piece;
-							chosen_hash[piece_name] = piece_object.pos
-						end
+			choice = gets.chomp.downcase
+			if valid_piece_name?(choice)
+				chosen_hash = {}
+				player.pieces.each do |piece_name,piece_object|
+					if piece_name =~ /#{choice}/
+						#print "piece_name, piece"; p piece_name; p piece;
+						chosen_hash[piece_name] = piece_object.pos
 					end
+				end
+				if chosen_hash.length > 0
 					break
 				else
 					puts "you don't have that piece :( choose a different one"
+					sleep(0.7)
 				end
 			else
 				puts "enter a valid piece name. you know what they are -__-"
 				sleep(0.7)
 			end
 		end
-		chosen_hash
+		return chosen_hash, choice
 	end
+
+	def choose_pos(chosen_hash,choice)
+		while 1
+			puts "there are #{choice}s at "
+			chosen_hash.values.each {|v| print "#{v} "}
+			puts ""
+			puts "which #{choice} do you want to move?"
+			pos = gets.chomp.downcase
+			if pos.length == 2 && (pos[0] =~ /[a-z]/) && (pos[1] =~ /\d/)
+				if chosen_hash.values.any? { |v| v == pos }
+					break
+				else
+					puts "you don't have a #{choice} on that square :("
+					sleep(0.5)
+				end	
+			else
+				puts "that's not a valid position :( try again"
+				sleep(0.7)
+			end
+		end
+		pos
+	end
+
 
 	def turn(player)
 		@board.show
 		puts "#{player.name}, your move!"
 		sleep(0.5)
-		chosen_hash = get_chosen_hash
+		chosen_arr = get_chosen_arr(player)
+		chosen_hash = chosen_arr[0]
+		choice = chosen_arr[1]
+		
 		if chosen_hash.length > 1
-
+			pos = choose_pos(chosen_hash,choice)
 		else
+			pos = chosen_hash.values[0]
 		end
+		puts pos
 	end
 
 	def valid_piece_name?(input)
